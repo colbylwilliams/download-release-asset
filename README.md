baf509cb60a02985ec609f3ab7a848c07cd500b7
Colby Williams <ColbyLWilliams@gmail.com>
2020-03-25 10:09:29 -0400

    v1

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ An Action to download a release asset via GitHub Release API
 
 ## Inputs
 
+### `token`
+
+**Required** The access token to access GitHub with.
+
 ### `asset_id`
 
 **Required** The ID of the uploaded asset to download.
@@ -12,8 +16,18 @@ An Action to download a release asset via GitHub Release API
 
 **Required** The path of the directory to save the downloaded asset.
 
+### `owner`
+
+**Optional** The owner of the repository.
+
+### `repo`
+
+**Optional** The name of the repository.
+
 ## Example usage
 
+```yml
 uses: colbylwilliams/download-release-asset@v1
 with:
   asset_id: '1234567'
+```
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const core = require('@actions/core');
-const { GitHub } = require('@actions/github');
+const github = require('@actions/github');
 const fs = require('fs');
-
 
 async function run() {
     try {
-        const github = new GitHub(process.env.GITHUB_TOKEN);
-
+        const token = core.getInput('token', { required: true });
         const assetId = core.getInput('asset_id', { required: true });
         const filePath = core.getInput('file_path', { required: true });
 
+        const userOwner = core.getInput('owner', { required: false })
+        const userRepo = core.getInput('repo', { required: false })
+
+        if ((userOwner && !userRepo) || (!userOwner && userRepo)) {
+            throw { message: "Must provide values for both owner and repo, or neither to use the actions context" };
+        }
+
+        const repo = {
+            owner: userOwner ?? github.context.repo.owner,
+            repo: userRepo ?? github.context.repo.repo,
+        };
+
+        const client = new github.GitHub(token ?? process.env.GITHUB_TOKEN);
+
         const headers = { 'Accept': 'application/octet-stream' };
 
-        const getAssetResponse = await github.repos.getReleaseAsset({
+        const getAssetResponse = await client.repos.getReleaseAsset({
             asset_id: assetId,
+            owner: repo.owner,
+            repo: repo.repo,
             headers
         });
 
