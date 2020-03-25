3b9634da182b2a3610f368a2404ff01242420bbc
Colby Williams <ColbyLWilliams@gmail.com>
2020-03-25 12:40:32 -0400

    get by name

--- README.md
+++ README.md
@@ -2,32 +2,38 @@
 
 An Action to download a release asset via GitHub Release API
 
-## Inputs
-
-### `token`
-
-**Required** The access token to access GitHub with.
-
-### `asset_id`
-
-**Required** The ID of the uploaded asset to download.
-
-### `file_path`
-
-**Required** The path of the directory to save the downloaded asset.
-
-### `owner`
-
-**Optional** The owner of the repository.
-
-### `repo`
-
-**Optional** The name of the repository.
-
-## Example usage
+## Usage
 
 ```yml
-uses: colbylwilliams/download-release-asset@v1
-with:
-  asset_id: '1234567'
+- uses: colbylwilliams/download-release-asset@v1
+  with:
+    # Repository name with owner. For example, colbylwilliams/download-release-asset
+    # Default: ${{ github.repository }}
+    repository: ''
+
+    # Personal access token (PAT) used to fetch the asset.
+    #
+    # We recommend using a service account with the least permissions necessary. Also
+    # when generating a new PAT, select the least scopes necessary.
+    #
+    # [Learn more about creating and using encrypted secrets](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)
+    #
+    # Default: ${{ github.token }}
+    token: ''
+
+    # The tag name of the release the asset is on. For example v1.0.0.
+    #
+    # This is required if the action was not triggered by a release event, whereas the
+    # release object in the event payload will be used.
+    tag: ''
+
+    # Relative path under $GITHUB_WORKSPACE to place the asset.
+    path: ''
+
+    # The name of the uploaded asset to download, including the extention.  For example, MyFile.zip
+    asset: ''
 ```
+
+## License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
--- index.js
+++ index.js
@@ -1,37 +1,95 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const path = require('path');
 
 async function run() {
     try {
-        const token = core.getInput('token', { required: true });
-        const assetId = core.getInput('asset_id', { required: true });
-        const filePath = core.getInput('file_path', { required: true });
 
-        const userOwner = core.getInput('owner', { required: false })
-        const userRepo = core.getInput('repo', { required: false })
+        let workspacePath = process.env['GITHUB_WORKSPACE']
+        if (!workspacePath)
+            throw new Error('GITHUB_WORKSPACE not defined.')
 
-        if ((userOwner && !userRepo) || (!userOwner && userRepo)) {
-            throw { message: "Must provide values for both owner and repo, or neither to use the actions context" };
+        workspacePath = path.resolve(workspacePath)
+
+        const repository = core.getInput('repository') || `${github.context.repo.owner}/${github.context.repo.repo}`
+        const splitRepository = repository.split('/')
+        if (splitRepository.length !== 2 || !splitRepository[0] || !splitRepository[1])
+            throw new Error(`Invalid repository '${repository}'. Expected format {owner}/{repo}.`)
+
+        const repo = { owner: splitRepository[0], repo: splitRepository[1] }
+
+        let assetPath = core.getInput('path', { required: false }) || '.'
+        assetPath = path.resolve(workspacePath, assetPath)
+        if (!(assetPath + path.sep).startsWith(workspacePath + path.sep))
+            throw new Error(`Asset path '${assetPath}' is not under '${workspacePath}'`)
+
+        const assetName = core.getInput('asset', { required: true });
+        if (!assetName)
+            throw new Error('Name must be provided.')
+
+        const token = core.getInput('token', { required: false }) || process.env['GITHUB_TOKEN']
+
+        const client = new github.GitHub(token)
+
+        let release;
+
+        if (github.context.eventName.toLowerCase() === 'release') {
+            release = github.context.payload.release
+            if (!release)
+                throw new Error('Unable to get release from event payload.')
+        } else {
+            const tag = core.getInput('tag', { required: false })
+            if (!tag)
+                throw new Error(`Tag is required for '${github.context.eventName} events. It may only be ommitted for 'release' events.`)
+
+            core.info('Getting release')
+            const releaseResponse = await client.repos.getReleaseByTag({
+                owner: repo.owner,
+                repo: repo.repo,
+                tag: tag
+            })
+
+            if (releaseResponse.status != 200)
+                throw new Error(`Unexpected response from GitHub API. Status: ${releaseResponse.status}, Data: ${releaseResponse.data}`)
+
+            release = releaseResponse.data
+
+            if (!release)
+                throw new Error('Unable to get release from GitHub API.')
         }
 
-        const repo = {
-            owner: userOwner ?? github.context.repo.owner,
-            repo: userRepo ?? github.context.repo.repo,
-        };
+        if (!Array.isArray(release.assets) || !release.assets.length)
+            throw new Error('The release has no assets.')
 
-        const client = new github.GitHub(token ?? process.env.GITHUB_TOKEN);
 
-        const headers = { 'Accept': 'application/octet-stream' };
+        const asset = release.assets.find(a => a.name === assetName)
 
-        const getAssetResponse = await client.repos.getReleaseAsset({
-            asset_id: assetId,
+        if (!asset)
+            throw new Error(`The release has no asset named ${assetName}.`)
+
+        const headers = { 'Accept': 'application/octet-stream' }
+
+        core.info('Downloading asset')
+        const assetResponse = await client.repos.getReleaseAsset({
+            asset_id: asset.id,
             owner: repo.owner,
             repo: repo.repo,
             headers
-        });
+        })
 
-        await fs.writeFile(filePath, getAssetResponse.data)
+        if (assetResponse.status == 300)
+            core.info('assetResponse.status == 300')
+
+        if (assetResponse.status != 200)
+            throw new Error(`Unexpected response from GitHub API. Status: ${assetResponse.status}, Data: ${assetResponse.data}`)
+
+        let fileData = Buffer.from(assetResponse.data) // response.data is ArrayBuffer
+
+        core.info('Writing asset to disk')
+        await fs.promises.writeFile(assetPath, fileData)
+
+        fileData = Buffer.from('') // Free memory
 
     } catch (error) {
         core.setFailed(error.message);
